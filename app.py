46afe41e1a04e9616fbf18dda2bd09583353385f
franfurey <franciscofurey@gmail.com>
2023-05-25 14:52:08 +0000

    Updates on project

--- app.py
+++ app.py
@@ -1,10 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id':1,
+    'title': 'Data Analyst',
+    'location': 'Cordoba, Argentina',
+    'salary': 'USD 2.000'
+  },
+  {
+    'id':2,
+    'title': 'Data Enginer',
+    'location': 'San Francisco, USA',
+    'salary': 'USD 3.000'
+  },
+  {
+    'id':3,
+    'title': 'Data Science',
+    'location': 'Remote'
+  }
+]
+
+
 @app.route('/')
 def hello():
-  return render_template('home.html')
+  return render_template('home.html',
+                         jobs=JOBS,
+                        company_name='Jovian')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
