f0c9445affa4647a8823cffc06b332ce11f7c591
franfurey <franciscofurey@gmail.com>
2023-06-05 19:52:39 +0000

    changes

--- app.py
+++ app.py
@@ -1,45 +1,22 @@
 from flask import Flask, render_template, jsonify
+from sqlalchemy import create_engine, text
+from database import engine, load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id':1,
-    'title': 'Data Analyst',
-    'location': 'Cordoba, Argentina',
-    'salary': 'USD 2.000'
-  },
-  {
-    'id':2,
-    'title': 'Data Enginer',
-    'location': 'San Francisco, USA',
-    'salary': 'USD 3.000'
-  },
-  {
-    'id':3,
-    'title': 'Data Science',
-    'location': 'Remote'
-  },
-  {
-    'id':4,
-    'title': 'Backend Python Developer',
-    'location': 'Remote',
-    'salary': 'USD 2.500'
-  }
-]
-
-
 @app.route('/')
 def hello():
+  jobs = load_jobs_from_db()
   return render_template('home.html',
-                         jobs=JOBS,
+                         jobs=jobs,
                         company_name='Jovian')
 
 
 @app.route("/api/jobs")
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
-  app.run(host = '0.0.0.0', debug = True)
+  app.run(host='0.0.0.0', debug=True, port=5002)
--- replit.nix
+++ replit.nix
@@ -4,5 +4,9 @@
         pkgs.cowsay
         pkgs.python3
         pkgs.python3Packages.flask
+        pkgs.python3Packages.sqlalchemy
+        pkgs.python3Packages.pymysql
+        pkgs.python3Packages.python-dotenv
+        pkgs.python3Packages.mysqlclient
     ];
 }
