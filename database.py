3db06dd3f63f637efdcc2da131a207fe96e2e9a8
franfurey <franciscofurey@gmail.com>
2023-06-05 20:23:40 +0000

    s

--- database.py
+++ database.py
@@ -12,9 +12,7 @@ engine = create_engine(db_connection_string,
 
 
 def load_jobs_from_db():
-  with engine.connect() as conn:
-    result = conn.execute(text("select * from jobs"))
-    jobs = []
-    for row in result.all():
-      jobs.append(dict(row.items()))
-    return jobs
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from jobs"))
+        jobs = [dict(row) for row in result]
+        return jobs
